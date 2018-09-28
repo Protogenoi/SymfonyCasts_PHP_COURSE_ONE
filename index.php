32ce0ddaf404bd2c5c80ee98c390c387512b92ea
Michael <michael@adl-crm.uk>
2018-09-28 12:05:12 +0100

    Completed Chapter 5: Arrays Level 3: We put Arrays in your Arrays!

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+
+$pet1 = array(
+    'name' => 'Chew Barka',
+    'breed' => 'Bichon',
+    'age' => '2 years',
+    'weight' => 8,
+    'bio' => 'The park, The pool or the Playground - I love to go anywhere! I am really great at... SQUIRREL!',
+    'filename' => 'pet1.png'
+);
+
+$pet2 = array(
+    'name' => 'Spark Pug',
+    'breed' => 'Pug',
+    'age' => '1.5 years',
+    'weight' => 11,
+    'bio' => 'You want to go to the dog park in style? Then I am your pug!',
+    'filename' => 'pet2.png'
+);
+
+$pet3 = array(
+    'name' => 'Pico de Gato',
+    'breed' => 'Bengal',
+    'age' => '5 years',
+    'weight' => 9,
+    'bio' => 'Oh hai, if you do not have a can of salmon I am not interested.',
+    'filename' => 'pet3.png'
+);
+
+$pancake = array(
+    'name' => 'Pancake the Bulldog',
+    'age' => '1 year',
+    'weight' => 9,
+    'bio' => 'Lorem Ipsum',
+    'filename' => 'pancake.png'
+);
+$pancake['breed'] = 'Bulldog';
+
+$pets = [$pet1, $pet2, $pet3, $pancake];
+$pets = array_reverse($pets);
+
+$cleverWelcomeMessage = 'All the love, none of the crap!';
+$pupCount = count($pets);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,121 +65,79 @@
 
 <body>
 
-    <div class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">AirPupNMeow</a>
-            </div>
-            <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li class="divider"></li>
-                            <li class="dropdown-header">Nav header</li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="navbar-form navbar-right">
-                    <div class="form-group">
-                        <input type="text" placeholder="Email" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" placeholder="Password" class="form-control">
-                    </div>
-                    <button type="submit" class="btn btn-success">Sign in</button>
-                </form>
-            </div>
-            <!--/.navbar-collapse -->
-        </div>
-    </div>
-
-    <div class="jumbotron">
-        <div class="container">
-
-
-            <?php
-
-            $cleverWelcomeMessage = 'All the love, none of the crap!';
-            $pupCount = rand(50, 100);
-
-            ?>
-
-            <h1><?php echo strrev(strtolower($cleverWelcomeMessage)); ?></h1>
-
-            <p><?php echo $pupCount; ?> pet friends!</p>
-
-            <p><a class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
-        </div>
-    </div>
-
-    <?php
-
-    $pet1 = 'Chew Barka';
-    $pet2 = 'Spark Pug';
-    $pet3 = 'Pico de Gato';
-
-    $pancake = [
-        'name' => 'Pancake',
-        'age' => '1 year',
-        'weight' => 9,
-        'bio' => 'Treas and Snoozin!',
-        'filename' => 'pancake.png'
-    ];
-
-    $pancake['breed'] = 'Bulldog';
-
-    $pets = [$pet1, $pet2, $pet3];
-    $pets[] = 'Kitty Gaga';
-
-    ?>
-
+<div class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
-        <div class="row">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">AirPupNMeow</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li class="divider"></li>
+                        <li class="dropdown-header">Nav header</li>
+                        <li><a href="#">Separated link</a></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="navbar-form navbar-right">
+                <div class="form-group">
+                    <input type="text" placeholder="Email" class="form-control">
+                </div>
+                <div class="form-group">
+                    <input type="password" placeholder="Password" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-success">Sign in</button>
+            </form>
+        </div>
+        <!--/.navbar-collapse -->
+    </div>
+</div>
+
+<div class="jumbotron">
+    <div class="container">
+
+        <h1><?php echo strtoupper(strtolower($cleverWelcomeMessage)); ?></h1>
+
+        <p><?php echo $pupCount; ?> pet friends!</p>
+
+        <p><a class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <?php foreach ($pets as $cutepet) { ?>
             <div class="col-lg-4 pet-list-item">
-                <h2><?php echo $pancake['name']; ?></h2>
-                <img src="/images/<?php echo $pancake['filename']; ?>"
+                <h2><?php echo $cutepet['name']; ?></h2>
+                <img src="/images/<?php echo $cutepet['filename']; ?>"
                      class="img-rounded"/>
 
                 <blockquote class="pet-details">
-                    <span class="label label-info"><?php echo $pancake['breed']; ?></span>
-                    <?php echo $pancake['age']; ?>
-                    <?php echo $pancake['weight']; ?> lbs
+                    <span class="label label-info"><?php echo $cutepet['breed']; ?></span>
+                    <?php echo $cutepet['age']; ?>
+                    <?php echo $cutepet['weight']; ?> lbs
                 </blockquote>
 
                 <p>
-                    <?php echo $pancake['bio']; ?>
+                    <?php echo $cutepet['bio']; ?>
                 </p>
             </div>
 
-            <?php
-
-            foreach ($pets as $cutePet) {
-                echo '<div class="col-lg-4">';
-                echo '<h2>';
-                echo $cutePet;
-                echo '</h2>';
-                echo '<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                    condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
-                    euismod. Donec sed odio dui. </p>';
-                echo '<p><a class="btn btn-default" href="#">View details &raquo;</a></p>';
-                echo '</div>';
-            }
-            ?>
-
+        <?php } ?>
         <hr>
 
         <footer>
