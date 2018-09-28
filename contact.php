f5fe519b7c8a0e4507526ad4dd1b17d026baf3e9
Michael <michael@adl-crm.uk>
2018-09-28 14:57:19 +0100

    Completed Chapter 11: Adding a very Simple Layout

--- contact.php
+++ contact.php
@@ -2,9 +2,12 @@
 
 require 'lib/functions.php';
 
+require 'layout/header.php';
 ?>
 
 <h1>
     Helping you find your new best friend from
     over <?php echo count(get_pets()); ?> pets.
 </h1>
+
+<?php require 'layout/footer.php';
