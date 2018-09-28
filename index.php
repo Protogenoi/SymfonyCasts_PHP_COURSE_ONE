17a1b1107b8214e9194b79c5aeb96abc6433d8bd
Michael <michael@adl-crm.uk>
2018-09-28 14:44:08 +0100

    Completed Chapter 10: Using require to Include Functions

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
 
-function get_pets()
-{
-
-    $petsJson = file_get_contents('data/pets.json');
-    $pets = json_decode($petsJson, true);
-
-    return $pets;
-
-}
+require 'lib/functions.php';
 
 $pets = get_pets();
 
