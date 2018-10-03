5e0cf38a903dd8043cf9e52a3abf7824beec92c1
Michael <michael@adl-crm.uk>
2018-10-03 13:10:16 +0100

    Course 2: Completed Chapter 1: Request, New Page and - Hey, You Welcome Back!

---
+++ pets_new.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * ------------------------------------------------------------------------
+ *                               ADL CRM
+ * ------------------------------------------------------------------------
+ *
+ * Copyright © 2018 ADL CRM All rights reserved.
+ *
+ * Unauthorised copying of this file, via any medium is strictly prohibited.
+ * Unauthorised distribution of this file, via any medium is strictly prohibited.
+ * Unauthorised modification of this code is strictly prohibited.
+ *
+ * Proprietary and confidential
+ *
+ * Written by michael <michael@adl-crm.uk>, 03/10/2018 13:05
+ *
+ * ADL CRM makes use of the following third party open sourced software/tools:
+ *  DataTables - https://github.com/DataTables/DataTables
+ *  EasyAutocomplete - https://github.com/pawelczak/EasyAutocomplete
+ *  PHPMailer - https://github.com/PHPMailer/PHPMailer
+ *  ClockPicker - https://github.com/weareoutman/clockpicker
+ *  fpdf17 - http://www.fpdf.org
+ *  summernote - https://github.com/summernote/summernote
+ *  Font Awesome - https://github.com/FortAwesome/Font-Awesome
+ *  Bootstrap - https://github.com/twbs/bootstrap
+ *  jQuery UI - https://github.com/jquery/jquery-ui
+ *  Google Dev Tools - https://developers.google.com
+ *  Twitter API - https://developer.twitter.com
+ *  Webshim - https://github.com/aFarkas/webshim/releases/latest
+ *
+ */
+
+require 'layout/header.php'; ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6">
+                <h1>Add your Pet! Squirrel!</h1>
+            </div>
+        </div>
+    </div>
+    HI I AM A PAGE!!!!
+
+    Squirrel!
+
+
+<?php require 'layout/footer.php';
