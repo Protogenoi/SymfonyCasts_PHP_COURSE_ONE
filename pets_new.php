8cec7d7eb9c72ecac317fa0a9995564fb7edd37f
Michael <michael@adl-crm.uk>
2018-10-03 14:19:25 +0100

    Course 2: Completed Chapter 2: We deserve to Create a Form

--- pets_new.php
+++ pets_new.php
@@ -33,14 +33,47 @@
 require 'layout/header.php'; ?>
     <div class="container">
         <div class="row">
-            <div class="col-xs-6">
+            <form class="col-xs-6">
                 <h1>Add your Pet! Squirrel!</h1>
-            </div>
+
+                <h1>Add your Pet</h1>
+
+                <form action="/pets_new.php" class="control-label"
+                      method="POST">
+
+                    <div class="form-group">
+                        <label for="pet-name" class="control-label">Pet
+                            Name</label>
+                        <input type="text" name="name" id="pet-name"
+                               class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="pet-breed"
+                               class="control-label">Breed</label>
+                        <input type="text" name="breed" id="pet-breed"
+                               class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="pet-weight"
+                               class="control-label">Weight</label>
+                        <input type="number" name="weight" id="pet-weight"
+                               class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="pet-bio" class="control-label">Pet
+                            Bio</label>
+                        <textarea name="bio" id="pet-bio"
+                                  class="form-control"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">
+                        <span class="glyphicon glyphicon-heart"></span> Add
+                    </button>
+
+                </form>
+
+        </div>
         </div>
     </div>
-    HI I AM A PAGE!!!!
-
-    Squirrel!
-
 
 <?php require 'layout/footer.php';
